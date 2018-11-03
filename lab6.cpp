96caafe345ab14450d4f817146e3868a142abd2e
Jones <jonesj10@wit.edu>
2018-11-03 13:56:41 -0400

    merge

--- lab6.cpp
+++ lab6.cpp
@@ -9,8 +9,13 @@
 #include <iostream>
 #include <string>
 #include <ctime>
+#include <ctime>
+#include <fstream>
 
 using namespace std;
+
+
+
 
 
 struct qb {
@@ -20,17 +25,41 @@ struct qb {
 
 } ;
 
+/*printmenu function that takes the users choice so that the choice may initialize t$
+function needed*/
+void printmenu(){
+	int choice =0;
+	cout << "please enter a 1 to import the data to the list" << endl;
+	cout << "Please enter a 2 to delete all of the entries on the list and exit" << endl;
+	cout << "Please enter a 3 to search for a struct" << endl;
+	cout << "please enter a 4 to delete a specific entry" << endl;
+	cout << "Please  enter a 5 to exit"; << endl;
+}
+//delete all function that deletes all of the entries
+void deleteall(){
+temp = HEAD;
+while(temp){
+        student *deleted =temp; //save the current
+        temp = temp->next; //moves to the next pointer
+        delete deleted;//deletes the current
+}//end of delete all
+
+
+
+
+
+void Searchfname(){
+	do
+
+	while(temp !0 HEAD)
+}
 
 
 int main{
-/*	struct qb {
-		str fname, lname;
-		int numWins; //starts a 0
-		int years[4]; //Most won by a QB in that span is 4
 
-	} ;
-*/
-
+	int choice =0;
+	printmenu();
+	cin >> choice;
 
 
 
